3c1c21eed6ff1ce5cb59851c41ed992ecd3299a8
Dragon <test@gmail.com>
2022-02-22 01:24:17 -0500

    Initial ToDo Commit

---
+++ ToDo/src/app.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Home from "./comp/Home.svelte";
+
+    export let name
+</script>
+
+<h1>
+    {name}'s ToDos
+</h1>
+
+<Home />
---
+++ ToDo/src/comp/Home.svelte
@@ -0,0 +1,9 @@
+<div>
+    <p>What would you like to do?</p>
+    <button>
+        Load
+    </button>
+    <button>
+        + New
+    </button>
+</div>
